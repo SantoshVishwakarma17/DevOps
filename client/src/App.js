b48a99780aabdd51a598c3636e7fa45fc81044b2
Santosh <santuvishwakarma17@gmail.com>
2025-06-01 11:32:12 +0530

    Save local changes before pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import LandingPage from './pages/LandingPage';
 import Authenticate from './pages/Authenticate';
